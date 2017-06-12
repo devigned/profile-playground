6a72d1d62bcb4c9b5da07b5da3cae241e96b1e91
David Justice <david@devigned.com>
2017-06-12 15:35:05 -0700

    add in runtime error

--- dotnet/samples/MultiProfileSample.cs
+++ dotnet/samples/MultiProfileSample.cs
@@ -17,19 +17,15 @@ public class MultiProfileSample {
         var newerAzure = new Profile_2017_05_15.Client();
         var newerPlan = newerAzure.Compute.Models.Plan(name: "planName", publisher: "publisher", product: "product", promotionCode: "code");
         vm = newerAzure.Compute.VirtualMachines.Create(name: "myVm", location: "westus", licenseType: "MIT", plan: newerPlan);
-        // // Properties available on VM in the 2017_05_15 profile
-        var newerId = vm.Id;
-        var newerProvisioningState = vm.ProvisioningState;
-        var newerLicenseType = vm.LicenseType;
-        var newerName = vm.Name;
-
-
         // var vmId = vm.VmId;  <--- still a compile error since vm is the older interface
 
         var newerVM = newerAzure.Compute.VirtualMachines.Create(name: "myVm", location: "westus", licenseType: "MIT", plan: newerPlan);
-        var vmId = newerVM.VmId;
+        var vmId = newerVM.VmId; // <--- this works, now that we have a newer VM interface
 
 
         olderAzure.Compute.VirtualMachines.Create(name: "myVm", location: "westus", licenseType: "MIT", plan: newerPlan);
+        vm = newerVM;
+
+        vm.Plan = plan; // <--- this will cause a runtime error because it can't project an old plan into the future
     }
 }
